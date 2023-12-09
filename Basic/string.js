f5b6ce1dd5bf58259242dcfc91df2fe84f1e7da9
Nishith Patel <96382694+patelnishith@users.noreply.github.com>
2023-12-09 10:25:11 +0000

    string

---
+++ Basic/string.js
@@ -0,0 +1,30 @@
+const firstName = "nishith ";
+const lastName = "patel";
+
+const middleName = String("patel raju pathak");
+const strArray = ["1", "2", "3"];
+
+console.log(firstName.charAt(1));
+console.log(firstName[1]);
+
+console.log(firstName.charCodeAt(1));
+console.log(firstName.at(1));
+console.log(firstName.codePointAt(1));
+console.log(firstName.length);
+console.log(middleName.split(" "));
+console.log(middleName.startsWith("patel"));
+
+// useful string function
+
+/* toUpperCase()
+ toLocaleLowerCase()
+ charAt()
+ indexOf()
+ lastIndexOf()
+ trim()
+ trimEnd()
+ trimStart()
+ concat()
+ slice()
+ split()
+ toString() */
