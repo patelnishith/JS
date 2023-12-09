ff98ff91938cc43bf9bc88ec4a98ddf755339cfe
Nishith Patel <96382694+patelnishith@users.noreply.github.com>
2023-12-09 10:39:30 +0000

    number

---
+++ Basic/number.js
@@ -0,0 +1,18 @@
+const num = Number(6.34535);
+
+console.log(Number.MIN_SAFE_INTEGER);
+
+// important
+
+Number.MAX_SAFE_INTEGER;
+Number.MIN_SAFE_INTEGER;
+Math.abs();
+Math.ceil();
+Math.floor();
+Math.max();
+Math.min();
+Math.trunc();
+Math.sqrt();
+Math.pow();
+Math.round();
+Math.random();
